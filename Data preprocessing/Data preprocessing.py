3b2dfa625d8293cb44a0f37b451845427d336b4b
Rushikesh Sangar <148414253+rushikesh-sangar@users.noreply.github.com>
2026-05-09 18:05:07 +0530

    Add files via upload

--- Data preprocessing/Data preprocessing.py
+++ Data preprocessing/Data preprocessing.py
@@ -33,7 +33,3 @@ y = labelencoder_y.fit_transform(y)
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state = 0)
-
-
-  
-
