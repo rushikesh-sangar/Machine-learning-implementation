5f6874e6e3d24a8d321ecc808054518c035c6280
Rushikesh Sangar <148414253+rushikesh-sangar@users.noreply.github.com>
2026-05-14 20:31:46 +0530

    Add files via upload

---
+++ Regression algorithm/Feature scaling/Feature scaling normalization.py
@@ -0,0 +1,42 @@
+# Feature scaling normalization
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(r'D:\Full Stack Data Science With Gen AI & Agentic AI Notes\Notes\May\1 May\Data.csv')
+
+# Independent vatiable
+x = dataset.iloc[:, :-1].values
+
+# Dependent variable
+y = dataset.iloc[:, 3].values.astype(object)
+
+# sklearn to fill missing numerical values
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer()
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+# Impute categorical values for independent variables
+from sklearn.preprocessing import LabelEncoder
+
+labelencoder_x = LabelEncoder()
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+
+# Impute categorical values for dependent variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Split the data
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state = 0)
+
+# Feature scaling normalization
+
+from sklearn.preprocessing import Normalizer
+nz_x = Normalizer()
+x_train = nz_x.fit_transform(x_train)
+x_test = nz_x.transform(x_test)
