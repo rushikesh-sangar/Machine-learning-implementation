5809caee40db4e1edcb51f7021843be9d7ecbb34
Rushikesh Sangar <148414253+rushikesh-sangar@users.noreply.github.com>
2026-05-18 11:55:11 +0530

    Add files via upload

--- Regression algorithm/Feature scaling/Feature scaling using standardization.py
+++ Regression algorithm/Feature scaling/Feature scaling using standardization.py
@@ -34,7 +34,7 @@ from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state = 0)
 
-# Feature scaling standardization
+# Feature scaling using standardization
 
 from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
