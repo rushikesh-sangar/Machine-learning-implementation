7aa233453b9321482e65c68fb34b0f88c9387fae
Rushikesh Sangar <148414253+rushikesh-sangar@users.noreply.github.com>
2026-05-28 18:02:50 +0530

    Add files via upload

--- Regression algorithm/Multiple linear regression/Multiple linear regression.py
+++ Regression algorithm/Multiple linear regression/Multiple linear regression.py
@@ -5,19 +5,19 @@ import pandas as pd
 
 dataset = pd.read_csv(r'D:\Full Stack Data Science With Gen AI & Agentic AI Notes\Notes\May\11 May\MLR\MLR investment\Investment.csv')
 
-x = dataset.iloc[:, :-1]
+X = dataset.iloc[:, :-1]
 y = dataset.iloc[:, 4]
 
-x = pd.get_dummies(x, dtype = int)
+X = pd.get_dummies(X, dtype = int)
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(x_train, y_train)
+regressor.fit(X_train, y_train)
 
-y_pred = regressor.predict(x_test)
+y_pred = regressor.predict(X_test)
 
 m = regressor.coef_
 print(m)
@@ -25,34 +25,33 @@ print(m)
 c = regressor.intercept_
 print(c)
 
-x = np.append(arr = np.full((50, 1), 42467).astype(int), values = x, axis = 1)
+X = np.append(arr = np.full((50, 1), 42467).astype(int), values = X, axis = 1)
 
 import statsmodels.api as sm
-x_opt = x[:, [0, 1, 2, 3, 4, 5]]
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
 
 # Ordinary least squares
-regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 3, 5]]
-regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+X_opt = X[:, [0, 1, 2, 3, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 2, 3]]
-regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+X_opt = X[:, [0, 1, 2, 3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1, 3]]
-regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+X_opt = X[:, [0, 1, 3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
 
-x_opt = x[:, [0, 1]]
-regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+X_opt = X[:, [0, 1]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
 
-bias = regressor.score(x_train, y_train)
+bias = regressor.score(X_train, y_train)
 print(bias)
 
-variance = regressor.score(x_test, y_test)
+variance = regressor.score(X_test, y_test)
 print(variance)
-
