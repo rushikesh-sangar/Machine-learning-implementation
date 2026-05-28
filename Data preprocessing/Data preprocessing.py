949f0a034f57b1ddd1e3ac2e0ee88fd99837c79a
Rushikesh Sangar <148414253+rushikesh-sangar@users.noreply.github.com>
2026-05-28 16:39:19 +0530

    Add files via upload

--- Data preprocessing/Data preprocessing.py
+++ Data preprocessing/Data preprocessing.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv(r'D:\Full Stack Data Science With Gen AI & Agentic AI Notes\Notes\May\1 May\Data.csv')
 
 # Independent vatiable
-x = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, :-1].values
 
 # Dependent variable
 y = dataset.iloc[:, 3].values.astype(object)
@@ -16,14 +16,14 @@ y = dataset.iloc[:, 3].values.astype(object)
 from sklearn.impute import SimpleImputer
 
 imputer = SimpleImputer()
-imputer = imputer.fit(x[:, 1:3])
-x[:, 1:3] = imputer.transform(x[:, 1:3])
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Impute categorical values for independent variables
 from sklearn.preprocessing import LabelEncoder
 
-labelencoder_x = LabelEncoder()
-x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 
 # Impute categorical values for dependent variable
 labelencoder_y = LabelEncoder()
@@ -32,4 +32,4 @@ y = labelencoder_y.fit_transform(y)
 # Split the data
 from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, test_size = 0.2, random_state = 0)
