b9451ae4e4fc322bcd047281761065a64baa78b9
Rushikesh Sangar <148414253+rushikesh-sangar@users.noreply.github.com>
2026-05-28 18:48:45 +0530

    Add files via upload

--- Regression algorithm/Polynomial regression/Polynomial regression.py
+++ Regression algorithm/Polynomial regression/Polynomial regression.py
@@ -6,35 +6,35 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv(r'D:\Full Stack Data Science With Gen AI & Agentic AI Notes\Notes\May\14 May\Polynomial regression\Employee salary.csv')
 
-x = dataset.iloc[:, 1:2].values
+X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 # Linear model
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
-lin_reg.fit(x, y)
+lin_reg.fit(X, y)
 
 # Polynomial model
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 5)
-x_poly = poly_reg.fit_transform(x)
+X_poly = poly_reg.fit_transform(X)
 
-poly_reg.fit(x_poly, y)
+poly_reg.fit(X_poly, y)
 
 lin_reg_2 = LinearRegression()
-lin_reg_2.fit(x_poly, y)
+lin_reg_2.fit(X_poly, y)
 
 # Linear regression visualization
-plt.scatter(x, y, color = 'red')
-plt.plot(x, lin_reg.predict(x), color = 'blue')
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Linear regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
 # Polynomial regression visualization
-plt.scatter(x, y, color = 'red')
-plt.plot(x, lin_reg_2.predict(poly_reg.fit_transform(x)), color = 'blue')
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
 plt.title('Truth or Bluff (Polynomial regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
@@ -46,4 +46,3 @@ print(lin_model_pred)
 
 poly_model_pred = lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
 print(poly_model_pred)
-
